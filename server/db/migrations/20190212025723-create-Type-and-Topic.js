d80c6210fc70725f312e6303e45b79c95b19b89c
Thuong Nguyen Thi Thu <thuthuong2304@gmail.com>
2019-02-14 09:22:27 +0700

    + create api doc and migrate type, topic

---
+++ server/db/migrations/20190212025723-create-Type-and-Topic.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('types', {
+          id: {autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
+          created_at: Sequelize.DATE,
+          updated_at: Sequelize.DATE,
+          name: {type: Sequelize.STRING},
+          parent_id: {
+              type: Sequelize.INTEGER,
+              references: {model: 'types', key: 'id'},
+          },
+      })
+          .then(function () {
+              return queryInterface.createTable('topics', {
+                  id: {autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
+                  created_at: Sequelize.DATE,
+                  updated_at: Sequelize.DATE,
+                  title: {type: Sequelize.STRING, allowNull: false},
+                  content: {type: Sequelize.STRING, allowNull: false},
+                  type_id: {
+                      type: Sequelize.INTEGER,
+                      references: {model: 'types', key: 'id'},
+                  },
+                  author_id: {
+                      type: Sequelize.INTEGER,
+                      references: {model: 'users', key: 'id'},
+                  },
+                  views: {type: Sequelize.INTEGER},
+                  likes: {type: Sequelize.INTEGER}
+              });
+          });
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('topics')
+          .then(function () {
+              return queryInterface.dropTable('types');
+          });
+  }
+};
