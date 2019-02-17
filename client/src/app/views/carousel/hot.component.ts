4d3aea5d04bcf455d32d8ba49647c2ae4c3cb10d
Lam Nguyen <sogoten6689@live.com>
2019-02-17 13:12:29 +0700

    - update ui client

---
+++ client/src/app/views/carousel/hot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hot-root',
+  templateUrl: 'carousel.component.html',
+  styleUrls: ["carousel.component.css"]
+})
+export class HotComponent {
+
+  constructor() { }
+
+}
---
+++ client/src/app/views/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-app',
+  templateUrl: 'login.component.html',
+  styleUrls: ["login.component.css"]
+})
+export class LoginComponent {
+
+  constructor() { }
+
+}
